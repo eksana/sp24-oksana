eb0465630d05c5414d20fda287075a5cf4872cc4
eksana <ser.vajnrub@yandex.ru>
2016-08-23 18:01:17 +0400

    ch

--- src/js/owl.js
+++ src/js/owl.js
@@ -2,10 +2,11 @@ $(document).ready(function() {
  
   $("#owl-demo").owlCarousel({
  
-      autoPlay: 3000, //Set AutoPlay to 3 seconds
+      //autoPlay: 3000, //Set AutoPlay to 3 seconds
  
       items : 2,
       touchDrag:true,
+
      itemsDesktop : [1000,2], //2 изображений на ширину между 1000px и 901px
       itemsDesktopSmall : [900,2], // 2 изображения между 900px и 601px
       itemsTablet: [600,2], //2 изображения между 600 и 0
