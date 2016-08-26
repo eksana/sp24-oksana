5c1ed271ed3f6e107b6bb14fc60dd8083f2bae08
eksana <ser.vajnrub@yandex.ru>
2016-08-26 11:06:20 +0400

    ch

--- src/js/common.js
+++ src/js/common.js
@@ -24,20 +24,55 @@ $(document).ready(function(){
 	/*-------------check form---------------------------*/
 
 
-
-	
-
-		$('.parent__label--fl').click(function(){
-			var $this_fl=$('.kottedj-wrap__single--fl');
-			$this_fl.fadeIn();
-			});
-		$('.parent__label--oz').click(function(){
+var $this_fl=$('.kottedj-wrap__single--fl');
 			var $this_oz=$('.kottedj-wrap__single--oz');
+			var $this_ol=$('.kottedj-wrap__single--ol');
+			var $this_pr=$('.kottedj-wrap__single--pr');
+	
+
+		$('.parent__label.parent__label--fl').click(function(){
+			/*var $this_fl=$('.kottedj-wrap__single--fl');
+			var $this_oz=$('.kottedj-wrap__single--oz');
+			var $this_ol=$('.kottedj-wrap__single--ol');
+			var $this_pr=$('.kottedj-wrap__single--pr');*/
+			$this_fl.fadeIn();
+			$this_oz.fadeOut();
+			$this_pr.fadeOut();
+			$this_ol.fadeOut();
 			
+						});
+
+		$('.parent__label.parent__label--oz').click(function(){
+			/*var $this_fl=$('.kottedj-wrap__single--fl');
+			var $this_oz=$('.kottedj-wrap__single--oz');
+			var $this_ol=$('.kottedj-wrap__single--ol');
+			var $this_pr=$('.kottedj-wrap__single--pr');*/
 			$this_oz.fadeIn();
+			$this_fl.fadeOut();
+			$this_ol.fadeOut();
+			$this_pr.fadeOut();
 			
 			});
+		$('.parent__label.parent__label--ol').click(function(){
+			/*var $this_fl=$('.kottedj-wrap__single--fl');
+			var $this_oz=$('.kottedj-wrap__single--oz');
+			var $this_ol=$('.kottedj-wrap__single--ol');
+			var $this_pr=$('.kottedj-wrap__single--pr');*/
+			$this_ol.fadeIn();
+			$this_oz.fadeOut();
+			$this_fl.fadeOut();
+			$this_pr.fadeOut();
+						});
 
-	
+	$('.parent__label.parent__label--pr').click(function(){
+			/*var $this_fl=$('.kottedj-wrap__single--fl');
+			var $this_oz=$('.kottedj-wrap__single--oz');
+			var $this_ol=$('.kottedj-wrap__single--ol');
+			var $this_pr=$('.kottedj-wrap__single--pr');*/
+			$this_pr.fadeIn();
+			$this_ol.fadeOut();
+			$this_oz.fadeOut();
+			$this_fl.fadeOut();
+						});
 /*---------------end document ready-----------*/		
 	});
